8bd2409cfdff0742f0a35082b33c95d93f8d1bc9
Ricard Roberg <ricardroberg@gmail.com>
2019-10-22 16:04:38 -0300

    Update models.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Learning, Knowledge, User, UserLogged
 
-# Register your models here.
+active_user = UserLogged
+
+@admin.register(Knowledge)
+class Knowledge(admin.ModelAdmin):
+    list_display = ('active', 'knowledge', 'create', 'last_modified')
+
+@admin.register(Learning)
+class LearningAdmin(admin.ModelAdmin):
+    list_display = ('active', 'learning', 'create', 'last_modified')
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'active', 'create', 'last_modified')
+
+
--- core/models.py
+++ core/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from django.contrib.auth import models as user_models
-import uuid
+from django.conf import settings
 from stdimage.models import StdImageField
+
+UserLogged = settings.AUTH_USER_MODEL
 
 # Using uuid to allow 'unlimited' file names stored (for pictures and posts)
 def get_file_path(_instance, filename):
@@ -10,11 +11,10 @@ def get_file_path(_instance, filename):
     return filename
 
 # Store who created what, when and when/who modified
-class Base(models.Model, user_models.User):
-    criade_by = user_models.User.get_username
+class Base(models.Model):
+    create_by = models.ForeignKey(UserLogged, default=1, null=True, on_delete=models.SET_NULL)
     create = models.DateField('Created', auto_now_add=True)
     last_modified = models.DateField('Last modified', auto_now=True)
-    last_modified_by = user_models.User.get_username
     active = models.BooleanField('Active?', default=True)
 
 # Still dont know that this is for
@@ -26,26 +26,19 @@ class Knowledge(Base):
     knowledge = models.CharField('Knowledge', max_length=50)
     description = models.TextField('Description', max_length=200)
 
-# You again
-    class Meta:
-        abstract = True
-
 # this is basicly a getter method
     def __str__(self):
         return self.knowledge
 
 # What im want to and willing to learn
 class Learning(Base):
-    learning = models.CharField('Knowledge', max_length=50)
+    learning = models.CharField('Learning', max_length=50)
     description = models.TextField('Description', max_length=200)
     date_start = models.DateField('When start', default=None)
 
-# I will unveil you little devil
-    class Meta:
-        abstract = True
-
     def __str__(self):
         return self.learning
+
 
 # Me, myself and Irene (Not actualy Irene, which i dont known, but any other user i will allow access
 class User(Base):
@@ -55,7 +48,6 @@ class User(Base):
     facebook = models.CharField('Facebook', max_length=100, default='#')
     twitter = models.CharField('Twitter', max_length=100, default='#')
     instagram = models.CharField('Instagram', max_length=100, default='#')
-
 
     class Meta:
         verbose_name = 'User'
